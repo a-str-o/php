fc535109f886804faea7124779eda38ef63ee05f
Younes Elguerraoui <yoelguer@e2r7p1.1337.ma>
2020-11-11 18:54:20 +0100

    n

---
+++ day01/ex09/ssap2.php
@@ -0,0 +1,36 @@
+#!/usr/bin/php
+<?php
+    
+    function ft_split($str)
+    {
+        $tab = array();
+        $i = 0;
+        $k = 0;
+        $j = 0;
+        while ($i < strlen($str))
+        {
+            while (($i < strlen($str)) &&  ($str[$i] == ' ' || $str[$i] == '\t'))
+                $i++;
+            $j = $i;
+            while (($j < strlen($str)) && ($str[$j] != ' ' && $str[$j] != '\t'))
+                $j++;
+            if ($i != $j)
+                $tab[$k] = substr($str, $i, $j-$i);
+            $i = $j;
+            $k++;
+        }
+        return $tab;
+    }
+    $i = 1;
+    $tab = array();
+    while ($i < $argc)
+    {
+        $s = ft_split($argv[$i]);
+        for ($l = 0 ; $l < count($s); $l++)
+            array_push($tab, $s[$l]);
+        $i++;
+    }
+    sort($tab );
+    for ($i = 1;$i < count($tab);$i++)
+        echo $tab[$i] ."\n";
+?>
